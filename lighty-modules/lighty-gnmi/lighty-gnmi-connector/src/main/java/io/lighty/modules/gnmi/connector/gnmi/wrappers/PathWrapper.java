875b1b6190dc1f415f48d7665c39f915c3eff3d4
Martin Bugan <martin.bugan@pantheon.tech>
2021-07-07 16:39:45 +0200

    Remove duplicate method from PathWrapper
    Reuse same method from GnmiHelper.
    
    Signed-off-by: Martin Bugan <martin.bugan@pantheon.tech>

--- lighty-modules/lighty-gnmi/lighty-gnmi-connector/src/main/java/io/lighty/modules/gnmi/connector/gnmi/wrappers/PathWrapper.java
+++ lighty-modules/lighty-gnmi/lighty-gnmi-connector/src/main/java/io/lighty/modules/gnmi/connector/gnmi/wrappers/PathWrapper.java
@@ -8,15 +8,11 @@
 
 package io.lighty.modules.gnmi.connector.gnmi.wrappers;
 
-import com.google.common.base.Strings;
-import gnmi.Gnmi;
+import static io.lighty.modules.gnmi.connector.gnmi.util.GnmiHelper.pathBuilder;
+
+import gnmi.Gnmi.Path.Builder;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import org.apache.commons.lang3.StringUtils;
 import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.mdsal.binding.dom.codec.impl.BindingCodecContext;
 import org.opendaylight.yangtools.yang.binding.DataObject;
@@ -29,23 +25,18 @@ import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes;
 import org.opendaylight.yangtools.yang.model.api.Module;
 
 public class PathWrapper {
-    private static final String REGEX_SLASH = "/";
-    private static final String REGEX_SEMICOLON = ";";
-    private static final Pattern KEY_VAL_PAIR = Pattern.compile("(?<key>(\\w.+))=(?<val>(\\w.+))");
-    private static final String REGEX_BEGIN_KEY = "[";
-    private static final String REGEX_END_KEY = "]";
     private final BindingCodecContext bindingCodecContext;
 
     public PathWrapper(BindingCodecContext bindingCodecContext) {
         this.bindingCodecContext = bindingCodecContext;
     }
 
-    public <T extends DataObject> Gnmi.Path.Builder getPathBuilder(InstanceIdentifier<T> instanceIdentifier)
+    public <T extends DataObject> Builder getPathBuilder(InstanceIdentifier<T> instanceIdentifier)
             throws IOException {
         return getPathBuilder(bindingCodecContext.toYangInstanceIdentifier(instanceIdentifier));
     }
 
-    public Gnmi.Path.Builder getPathBuilder(YangInstanceIdentifier instanceIdentifier) throws IOException {
+    public Builder getPathBuilder(YangInstanceIdentifier instanceIdentifier) throws IOException {
         NormalizedNode<?, ?> normalizedNode =
                 ImmutableNodes.fromInstanceId(this.bindingCodecContext.getRuntimeContext().getEffectiveModelContext(),
                         instanceIdentifier);
@@ -59,39 +50,6 @@ public class PathWrapper {
             return pathBuilder(instanceIdentifierNodeStreamWriter.getUrl());
         }
         return null;
-    }
-
-    public static Gnmi.Path.Builder pathBuilder(String xpath) {
-        final Gnmi.Path.Builder pathBuilder = Gnmi.Path.newBuilder();
-        if (!Strings.isNullOrEmpty(xpath)) {
-            String[] tokens = xpath.split(REGEX_SLASH);
-            for (String token : tokens) {
-                if (!StringUtils.isBlank(token)) {
-                    String elem;
-                    Map<String, String> keys = new HashMap<>();
-                    final int beginKey = token.indexOf(REGEX_BEGIN_KEY);
-                    final int endKey = token.indexOf(REGEX_END_KEY);
-                    if (beginKey > 0) {
-                        elem = token.substring(0, beginKey);
-                        String keyPeers = token.substring(beginKey + 1, endKey);
-                        for (String keyValPair : keyPeers.split(REGEX_SEMICOLON)) {
-                            final Matcher matcher = KEY_VAL_PAIR.matcher(keyValPair);
-                            if (matcher.matches()) {
-                                keys.put(matcher.group("key"), matcher.group("val"));
-                            }
-                        }
-                    } else {
-                        elem = token;
-                    }
-                    gnmi.Gnmi.PathElem.Builder elemBuilder = Gnmi.PathElem.newBuilder().setName(elem);
-                    if (!keys.isEmpty()) {
-                        elemBuilder.putAllKey(keys);
-                    }
-                    pathBuilder.addElem(elemBuilder.build());
-                }
-            }
-        }
-        return pathBuilder;
     }
 
     private String getPrefix(YangInstanceIdentifier instanceIdentifier) {
